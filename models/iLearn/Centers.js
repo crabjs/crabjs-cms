7fcd17eedb556ea52f3e5ec026fc510c51ec536b
Hai <lephuhai0511@gmail.com>
2016-11-29 08:07:23 +0700

    Update ilearn crm

---
+++ models/iLearn/Centers.js
@@ -0,0 +1,30 @@
+/**
+ * @license
+ * Copyright (c) 2016 The {life-parser} Project Authors. All rights reserved.
+ * This code may only be used under the MIT style license found at http://100dayproject.github.io/LICENSE.txt
+ * The complete set of authors may be found at http://100dayproject.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at http://100dayproject.github.io/CONTRIBUTORS.txt
+ * Code distributed by 100dayproject as part of the life.
+ * Created by Hai on 11/17/2016.
+ */
+
+"use strict";
+
+let mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+let Class = new Schema({
+    name: {type: String, required: true, trim: true},
+    alias: {type: String, maxlength: 255, trim: true, lowercase: true},
+    description: {type: String, default: ''},
+    status: {type: Number, default: 0}
+}, {
+    timestamps: {
+        createdAt: 'created_at',
+        updatedAt: 'updated_at'
+    },
+    collection: 'class'
+});
+
+module.exports = mongoose.model('class', Class);
+ 
